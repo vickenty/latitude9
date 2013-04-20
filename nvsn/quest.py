05a2c42355611f9e53283adc8bc0d0fbbd60bde8
Robert Nilsson <rn@orbstation.com>
2013-04-21 01:13:18 +0200

    Refill types so we can keep on playing\!

--- nvsn/quest.py
+++ nvsn/quest.py
@@ -2,16 +2,18 @@ import random
 import items
 
 class Quest (object):
+    types = None
     def __init__(self, goals):
         self.goals = goals
 
     @classmethod
     def new_random(self, size=3):
-        types = items.GoalItem.types
+        if Quest.types is None or len(Quest.types) == 0:
+            Quest.types = list(items.GoalItem.types)
         goals = []
         for i in range(size):
-            goal = random.choice(types)
-            types.remove(goal)
+            goal = random.choice(Quest.types)
+            Quest.types.remove(goal)
             goals.append(goal)
 
         return self(goals)
